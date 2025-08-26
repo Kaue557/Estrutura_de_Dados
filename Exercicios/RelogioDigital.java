1ccea2235db1569062d05f5adfb21d0e536ad77c
Kauê Lima <kauelima557@gmail.com>
2025-08-26 13:01:18 +0000

    2608

---
+++ Exercicios/RelogioDigital.java
@@ -0,0 +1,34 @@
+package Exercicios;
+
+public class RelogioDigital {
+        // atributos
+        private int hora;
+        private int minuto;
+        private int segundo;
+
+        //getters e setters
+        public inte getHora(){
+            return hora;
+        }
+
+        public void setHora(int hora){
+            if()
+            this.hora = hora;
+        }
+    public static void main(String[] args){
+
+    }
+
+    // construtor
+    public RelogioDigital(){
+        hora = 0;
+        minuto = 0;
+        segundo = 0;
+    }
+    // construtor
+    public RelogioDigital(int hora, int minuto, int segundo){
+        this.hora;
+        this.minuto;
+        this.segundo;
+    }
+}
