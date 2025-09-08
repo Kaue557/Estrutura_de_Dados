56b67739818ac7429a62a1b25fc137f99d1750a4
Kauê Lima <kauelima557@gmail.com>
2025-09-08 15:00:14 +0000

    0809

---
+++ Pratica/PilhaEx5.java
@@ -0,0 +1,16 @@
+/*
+Considere o trecho de código abaixo para a pilha P (tam. 100) com valores
+numéricos inteiros positivos. Faça um programa que remova todos os múltiplos
+de 3 de P1 e mantenha a mesma ordem dos elementos não múltiplos.
+ */
+
+public class PilhaEx5 {
+    // metodos
+    
+
+
+    public static void main(String[] args){
+
+
+    }
+}
